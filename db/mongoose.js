d10061a0b0dc83946ee9e3408c61394a5945d4ce
Kashish Mittal <kashkind7@gmail.com>
2019-09-07 18:09:22 +0530

    dotenv added

--- db/mongoose.js
+++ db/mongoose.js
@@ -7,6 +7,7 @@ mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser : true});
 
 
+
 var db = mongoose.connection;
 db.once('open' , ()=>{
     console.log('Connected to MongoServer');
